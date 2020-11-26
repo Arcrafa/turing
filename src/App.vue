e9a9349c61d8b22848bd1cc9ef551e38a94bfe89
rafa <maldonadoagudelorafael@gmail.com>
2020-11-26 02:23:53 -0500

    grafo y editor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Editor></Editor>
+    <Grafo></Grafo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import Editor from "@/components/Editor";
+import Grafo from "@/components/Grafo";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    Editor,
+    Grafo
   }
 }
 </script>
