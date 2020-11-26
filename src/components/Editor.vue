0a3852b599bafacb3678e655699d43ce65c7a9ba
rafa <maldonadoagudelorafael@gmail.com>
2020-11-26 02:56:54 -0500

    grafo y editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div>
+    <AceEditor
+        :fontSize="14"
+        :showPrintMargin="true"
+        :showGutter="true"
+        :highlightActiveLine="true"
+        mode="json"
+        theme="monokai"
+        :onChange="onChange"
+        name="editor"
+        :editorProps="{$blockScrolling: true}"
+    />
+
+  </div>
+</template>
+
+<script>
+
+import {Ace as AceEditor} from 'vue2-brace-editor';
+
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
+
+export default {
+  name: 'Editor',
+  data: {
+    automata: {
+      ini: 'Q0',
+      estados: [
+        {
+          nombre: 'Q0',
+          transiciones: [{
+            lee: ['a', 'b'],
+            escribe: 'a',
+            mueve: 'D',
+            cambia: 'Q0'
+          }, {
+            lee: [' '],
+            escribe: ' ',
+            mueve: 'D',
+            cambia: 'DONE'
+          }]
+        }, {}
+      ],
+      acept: 'done'
+
+    }
+  },
+  components: {
+    AceEditor,
+  },
+  $el: 'root',
+  methods: {
+    onChange(newValue) {
+      console.log('change', newValue);
+    }
+  }
+}
+</script>
---
+++ src/components/Grafo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>grafo</h1>
+    <div id="mynetwork"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Grafo",
+  data: {},
+
+
+  mounted() {
+    this.grafo()
+  },
+  methods: {
+    grafo: function (e) {
+      // create an array with nodes
+      var nodes = new vis.DataSet([
+        {id: 1, label: 'QO'},
+        {id: 2, label: 'aceptacion'},
+
+      ]);
+
+      // create an array with edges
+      var edges = new vis.DataSet([
+        {from: 1, to: 1, arrows: "to", label: "a,b -> a,R", font: {align: "bottom"}},
+        {from: 1, to: 2, arrows: "to", label: "' ',-> R", font: {align: "bottom"}}
+      ]);
+
+      // create a network
+      var container = document.getElementById('mynetwork');
+
+      // provide the data in the vis format
+      var data = {
+        nodes: nodes,
+        edges: edges
+      };
+      var options = {};
+
+      // initialize your network!
+      var network = new vis.Network(container, data, options);
+      console.log(network)
+    }
+  }
+}
+</script>
+
+<style escoped>
+#mynetwork {
+  width: 600px;
+  height: 400px;
+  border: 1px solid lightgray;
+}
+</style>
